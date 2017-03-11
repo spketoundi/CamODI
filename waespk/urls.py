b1730b06b14f4e1703987e2775d4ad0cef4794fd
spketoundi <simonpierrekisitoetoundi@gmail.com>
2017-03-11 18:33:42 +0100

    Add files via upload

---
+++ waespk/urls.py
@@ -0,0 +1,49 @@
+from django.conf.urls import include, url
+from django.conf import settings
+from django.contrib import admin
+from djgeojson.views import GeoJSONLayerView
+
+from wagtail.contrib.wagtailsitemaps.views import sitemap
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+
+from waespk.core import urls as ossuo_urls
+
+
+urlpatterns = [
+    url(r'^django-admin/', include(admin.site.urls)),
+
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^sitemap\.xml$', sitemap),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView, RedirectView
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Add views for testing 404 and 500 templates
+    urlpatterns += [
+        url(r'^test404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^test500/$', TemplateView.as_view(template_name='500.html')),
+    ]
+
+    # Favicon
+    urlpatterns += [
+        url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'ossuo.com/images/favicon.ico')),
+    ]
+
+
+urlpatterns += [
+    url(r'', include(ossuo_urls)),
+    url(r'', include(wagtail_urls)),
+]
+
+handler404 = 'waespk.core.views.error404'
